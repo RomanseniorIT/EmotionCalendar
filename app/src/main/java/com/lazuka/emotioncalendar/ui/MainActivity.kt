7fd01355043c1e9bda8b9e53537d7b8141569ae9
Roman Lazuko <lazuko.romann@gmail.com>
2024-12-14 14:34:30 +0300

    Setup navigation

---
+++ app/src/main/java/com/lazuka/emotioncalendar/ui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.lazuka.emotioncalendar.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.lazuka.emotioncalendar.R
+import com.lazuka.emotioncalendar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
